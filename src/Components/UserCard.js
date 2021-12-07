b490e00dcaccc1c49b8e09d749d3a29440b17bf2
Pratik Dutta <pratikdutta.786@gmail.com>
2021-12-07 23:56:59 +0530

    create usercard

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./index.module.css";
 
 const UserCard = () => {
-  return <>UserCard</>;
+  return (
+    <div className={styles.userCardBox}>
+      Pratik Dutta
+      <img
+        className={styles.userCardImg}
+        src="https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      ></img>
+    </div>
+  );
 };
 
 export default UserCard;
--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,11 +1,13 @@
 import React from "react";
+import styles from "./index.module.css";
 import UserCard from "./UserCard";
 
 const UserList = () => {
   return (
-    <>
+    <div className={styles.userlistBox}>
+      <h1>Users</h1>
       <UserCard />
-    </>
+    </div>
   );
 };
 
